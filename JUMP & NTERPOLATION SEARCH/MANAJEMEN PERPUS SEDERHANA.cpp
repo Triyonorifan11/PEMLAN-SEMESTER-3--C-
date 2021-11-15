b3a3d98ffc9894950a882b36f05586041bbb65a5
Triyono Rifan <triyonorifan01@gmail.com>
2021-11-15 22:18:21 +0700

    update manajemen perpus sederhana

--- JUMP & NTERPOLATION SEARCH/MANAJEMEN PERPUS SEDERHANA.cpp
+++ JUMP & NTERPOLATION SEARCH/MANAJEMEN PERPUS SEDERHANA.cpp
@@ -5,8 +5,9 @@
 
 struct buku{
 	long long int kode;
-	char *pengarang;
-	char *judul;
+	char pengarang[25];
+	char judul[25];
+	int convert_string;
 }buku[10];
 //typedef struct buku Perpus_buku[10];
 
@@ -18,7 +19,7 @@ int l = 0;
 void MainMenu();
 void daftarBuku();
 void tampil_buku();
-void daftar_buku();
+void input_buku();
 void Sorting();
 void TampilSort();
 void CariKode();
@@ -31,13 +32,12 @@ void MainMenu(){
 	system("cls");
 	printf("\t\t\t\t\t\t\t=============== MENU UTAMA ===============\n");
 	printf("\t\t\t\t\t\t\t====== Aplikasi Manajemen Buku perpustakaan ======\n");
-//	TampilData2();
+
 	if(l == 0){
-		daftar_buku();
+		input_buku();
 		tampil_buku();
 	}else{
-		Sorting();
-		TampilSort();
+		tampil_buku();
 	}
 	
 	printf("\n\t\tPilih Aksi:\n");
@@ -48,16 +48,35 @@ void MainMenu(){
 	printf("\n\n\t\tSilahkan Pilih = ");
 	scanf("%d", &pilih);
 	if(pilih == 1){
-	CariKode();
+		CariKode();
+	}
+	else if(pilih == 2){
+		CariJudul();
+	}
+	else if(pilih == 3){
+		close();
+	}
+}
+	
+void daftarBuku(long long int kode, const char judul[25], const char pengarang[25]){
+	buku[j].kode = kode;
+	strcpy(buku[j].judul, judul);
+	strcpy(buku[j].pengarang, pengarang);
+	j++;
+}
+	
+void input_buku(){
+	daftarBuku(3031, "C Programing", "James Lee");
 	daftarBuku(101, "Wordpress Expert", "Andrew Octa");
 	daftarBuku(8812, "Web From Zero to Zero", "David Guetta");
 	daftarBuku(790, "Laravel", "Hans Jackson");
-	daftarBuku(66, "Master Futter", "Ozur Haskels");
+	daftarBuku(66, "Master Flutter", "Ozur Haskels");
 	daftarBuku(111, "CodeIgniter 4 the guide", "Gantz Patrick");
 	daftarBuku(678, "Raspberry Pi 4", "Tony Watts");
 	daftarBuku(901, "Building Robotic AI", "Leni Imelda");
 	daftarBuku(303, "Data Science with Python", "Ranya Sundar");
 	daftarBuku(4089, "Go Lang Introduction", "Brian Macakzie");
+	
 }
 
 
@@ -80,6 +99,7 @@ void tukar(struct buku *a, struct buku *b){
 	*b = t;
 }
 
+//Urut berdasarkan Kode
 void Sorting(){
 	int index = 0;
 	for(int i = 0; i<j; i++){
@@ -94,17 +114,7 @@ void Sorting(){
 	}
 }
 
-void TampilSort(){
-	int i = 0;
-	//printf("===== Hasil Sorting =====\n\n");
-	while(i<j){
-		printf("%d  Kode Buku\t = %lld\n", i+1, buku[i].kode);
-		printf("   Judul buku\t = %s\n", buku[i].judul);
-		printf("   Pengarang\t = %s\n\n", buku[i].pengarang);
-		i++;
-	}
-}
-
+//Cari berdasarka Kode
 void jumpsearch(struct buku buku[], int lenght, long long int Kode){
 	
 	//langkah untuk jump
@@ -159,36 +169,95 @@ void CariKode(){
 	MainMenu();
 }
 
-int interpolationSearch(struct buku buku[], int lenght, char* key){
-	int posisi, low, high;
-	low = 0;
-	high = lenght-1;
-	
-	while((strcmp(buku[low].judul, key) > 0) && (strcmp(buku[high].judul, key))){
-		posisi = (key - buku[low]) / (buku[high] - buku[low]) * (high - low) + low;
-		if((strcmp(buku[posisi], key) == 0)){
-			return posisi;
+//Urut Berdasarkan Nama
+void SortNamaBubble(){
+	for(int i = 0; i<j; i++){
+		int k=i;
+		for(int x=i+1; x<j; x++){
+			int xy = strcmp(buku[k].judul, buku[x].judul);
+			if(xy > 0){
+				k = x;
+			}
+		}
+		if(k != i){
+			tukar(&buku[k], &buku[i]);
 		}
 	}
-	
 }
 
+//Cari berdasarkan Nama
+void interpolationSearch(){
+	printf("Mohon maaf belum paham dengan interpolation search pada string");
+//	int i,c,d,j;
+//	int a = sizeof(buku)/sizeof(buku[0]);
+//	int length[a];
+//	
+//	 for ( i = 0; i < a; i++){
+//        length[i] = strlen(buku[i].judul);
+//        
+//        for ( j = 0; j < length[i]; j++){
+//            buku[i].convert_string += buku[i].judul[j];
+//        }
+//        printf("%d. %d\n",i+1, buku[i].convert_string);
+//    }
+//	
+//	char string_cari[50];
+//    int convert_string = 0;
+//    printf("Masukkan Judul Buku yang ingin dicari : ");
+//    fflush(stdin); gets(string_cari);
+//    
+//    int  panjang = strlen(string_cari);
+//    for ( i = 0; i < panjang; i++){
+//       	convert_string += string_cari[i];
+//    }
+//    
+//    for ( c = 0; c < 10; c++){
+//        int temp;
+//        temp = buku[c].convert_string;
+//        d = c-1;
+//            while (d>=0 && buku[d].convert_string > temp){
+//                buku[d+1].convert_string = buku[d].convert_string;
+//                d = d-1;
+//            }
+//        buku[d+1].convert_string = temp;
+//    }
+//	
+//	
+//    int low = 0;
+//    int high = a - 1;
+//    int position;
+//
+//    while (low <= high){
+//
+//        position = low+(((convert_string-buku[low].convert_string)/(buku[high].convert_string-buku[low].convert_string))*(high-low));
+//
+//        if(buku[position].convert_string == convert_string){
+//          	printf("\n======= Data Ditemukan =========\n");
+//			printf("%d  Kode Buku\t = %lld\n", i+1, buku[i].kode);
+//			printf("   Judul buku\t = %s\n", buku[i].judul);
+//			printf("   Pengarang\t = %s\n\n", buku[i].pengarang);
+//            break;
+//        }else if(buku[position].convert_string > convert_string){
+//            high = position - 1;
+//        }else{
+//            low = position + 1;
+//        }
+//    }
+//    
+}
+//cari Judul buku
 void CariJudul(){
-	char judulCari;
-	
-	int lenght = sizeof(buku)/sizeof(buku[0]);
-	printf("Masukkan Judul buku = ");
-	scanf("%s", &judulCari);
-	fflush(stdin);
-	Sorting();
+
+	SortNamaBubble();
 	l++;
 	
-	interpolationSearch(buku, lenght, &judulCari);
+	interpolationSearch();
 	
 	system("pause");
 	MainMenu();
 }
 
+//Tutup apk
 void close(){
 	system("cls");
 	system("color 1f");
