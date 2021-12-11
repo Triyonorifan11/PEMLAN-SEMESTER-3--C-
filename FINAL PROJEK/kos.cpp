5d9a6f151271ff793f4332d9e71c25619e24f589
Triyono Rifan <triyonorifan01@gmail.com>
2021-12-11 10:31:21 +0700

    update apk kos FP

--- FINAL PROJEK/kos.cpp
+++ FINAL PROJEK/kos.cpp
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 
 struct lokasi_kos{
-	char provinsi[30];
-	char Kabupaten[30];
-	char jalan[50];
+	char provinsi[100];
+	char Kabupaten[130];
+	char jalan[150];
 };
 
 struct pemilik_kos{
@@ -12,13 +12,14 @@ struct pemilik_kos{
 	lokasi_kos lokasi;
 	char fasilitas[300];
 	char tipe[15];
-}Kos[100];
+}Kos;
 
 //inisialisasi
 void Pemilik_Kos();
 void Penyewa_Kos();
 void input_kos();
 void update_kos();
+void lihat_kos();
 
 int jumlah_kos = 0;
 
@@ -62,6 +63,8 @@ void Pemilik_Kos(){
 		case 2:
 			update_kos();
 			break;
+		case 3:
+			lihat_kos();
 	}
 	
 }
@@ -78,30 +81,47 @@ void Penyewa_Kos(){
 
 void input_kos(){
 	FILE *fptr;
-	fptr = fopen("Ummik_Kos.txt","a");
+	fptr = fopen("Ummik_Kos.txt","a"); //menghasulkan pointer
 	
 	printf("===== Masukkan inputan dibawah ini =====\n");
 	printf("Masukkan tipe rumah (LK/P) = ");
-	scanf("%s", &Kos[jumlah_kos].tipe);
+	scanf("%s", &Kos.tipe);
 	printf("Masukkan alamat:");
 	printf("\n\tProvinsi = ");
-	scanf(" %[^\n]s", &Kos[jumlah_kos].lokasi.provinsi);
+	scanf(" %[^\n]s", &Kos.lokasi.provinsi);
 	printf("\tKabupaten = ");
-	scanf(" %[^\n]s", &Kos[jumlah_kos].lokasi.Kabupaten);
+	scanf(" %[^\n]s", &Kos.lokasi.Kabupaten);
 	printf("\tJalan = ");
-	scanf(" %[^\n]s", &Kos[jumlah_kos].lokasi.jalan);
+	scanf(" %[^\n]s", &Kos.lokasi.jalan);
 	printf("Harga = ");
-	scanf("%lld", &Kos[jumlah_kos].harga);
+	scanf("%lld", &Kos.harga);
 	printf("Fasilitas = ");
-	scanf(" %[^\n]s", &Kos[jumlah_kos].fasilitas);
+	scanf(" %[^\n]s", &Kos.fasilitas);
 	
-	fprintf(fptr,"%s %s %s %s %lld %s", Kos[jumlah_kos].tipe, Kos[jumlah_kos].lokasi.provinsi, Kos[jumlah_kos].lokasi.Kabupaten, Kos[jumlah_kos].lokasi.jalan, Kos[jumlah_kos].harga, Kos[jumlah_kos].fasilitas);
+//	while(fscanf(fptr," %s %s %s %s %lld %s", &Kos.tipe, &Kos.lokasi.provinsi, &Kos.lokasi.Kabupaten, &Kos.lokasi.jalan, &Kos.harga, &Kos.fasilitas)!=EOF){
+//		jumlah_kos++;
+//	}
+	
+	fprintf(fptr,"%s %s %s %s %lld %s \n", Kos.tipe, Kos.lokasi.provinsi, Kos.lokasi.Kabupaten, Kos.lokasi.jalan, Kos.harga, Kos.fasilitas);
 	fclose(fptr);
 	
-	jumlah_kos++;
-	mainMenu();
+	system("pause");
+	Pemilik_Kos();
 }
-
+void lihat_kos(){
+	FILE *view;
+	int hasil = 0;
+	view = fopen("Ummik_Kos.txt","r");
+	
+	while(fscanf(view,"  %[^\n]s  %[^\n]s  %[^\n]s  %[^\n]s %lld  %[^\n]s", &Kos.tipe, &Kos.lokasi.provinsi, &Kos.lokasi.Kabupaten, &Kos.lokasi.jalan, &Kos.harga, &Kos.fasilitas)!=EOF){
+		printf("%s %s %s %s %lld %s\n", Kos.tipe, Kos.lokasi.provinsi, Kos.lokasi.Kabupaten, Kos.lokasi.jalan, Kos.harga, Kos.fasilitas);
+		jumlah_kos++;
+	}
+	
+	printf("jumlah banyak kos %d\n\n", hasil);
+	fclose(view);
+system("pause");
+}
 void update_kos(){
 	printf("Update Kos");
 }
